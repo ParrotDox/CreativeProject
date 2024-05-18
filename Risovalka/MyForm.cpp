d946f4054068c9bc4dfe8cffb3e20b37784362e9
Bibub <slappierR@gmail.com>
2024-05-18 17:52:14 +0500

    Recreated project again, added header

---
+++ Risovalka/MyForm.cpp
@@ -0,0 +1,18 @@
+﻿#include "MyForm.h"
+
+//Для работы с формами
+using namespace System;
+using namespace System::Windows::Forms;
+
+//Говорит о том, где искать точку входа программе
+[STAThreadAttribute]
+
+void main(array<String^>^ args)
+{
+	//Стандартные параметры запуска
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	//Запуск
+	Risovalka::MyForm form;
+	Application::Run(% form);
+}
---
+++ Risovalka/MyForm.h
@@ -0,0 +1,370 @@
+﻿#pragma once
+
+namespace Risovalka {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^ создатьToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ сохранитьКакToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ очиститьToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ настройкиЦветаToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ свободныйToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ ступенчатыйToolStripMenuItem;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::GroupBox^ groupBox4;
+	private: System::Windows::Forms::GroupBox^ groupBox3;
+	private: System::Windows::Forms::GroupBox^ groupBox2;
+	private: System::Windows::Forms::SplitContainer^ splitContainer2;
+	private: System::Windows::Forms::SplitContainer^ splitContainer1;
+	private: System::Windows::Forms::GroupBox^ groupBox5;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button1;
+
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->toolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->создатьToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->сохранитьКакToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->очиститьToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->настройкиЦветаToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->свободныйToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ступенчатыйToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->splitContainer2 = (gcnew System::Windows::Forms::SplitContainer());
+			this->groupBox4 = (gcnew System::Windows::Forms::GroupBox());
+			this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
+			this->groupBox5 = (gcnew System::Windows::Forms::GroupBox());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->menuStrip1->SuspendLayout();
+			this->groupBox1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer2))->BeginInit();
+			this->splitContainer2->SuspendLayout();
+			this->groupBox4->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->BeginInit();
+			this->splitContainer1->Panel2->SuspendLayout();
+			this->splitContainer1->SuspendLayout();
+			this->groupBox5->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->toolStripMenuItem1,
+					this->настройкиЦветаToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Padding = System::Windows::Forms::Padding(5, 2, 0, 2);
+			this->menuStrip1->Size = System::Drawing::Size(1262, 28);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// toolStripMenuItem1
+			// 
+			this->toolStripMenuItem1->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->создатьToolStripMenuItem,
+					this->сохранитьКакToolStripMenuItem, this->очиститьToolStripMenuItem
+			});
+			this->toolStripMenuItem1->Name = L"toolStripMenuItem1";
+			this->toolStripMenuItem1->Size = System::Drawing::Size(62, 24);
+			this->toolStripMenuItem1->Text = L"Холст";
+			this->toolStripMenuItem1->Click += gcnew System::EventHandler(this, &MyForm::toolStripMenuItem1_Click);
+			// 
+			// создатьToolStripMenuItem
+			// 
+			this->создатьToolStripMenuItem->Name = L"создатьToolStripMenuItem";
+			this->создатьToolStripMenuItem->Size = System::Drawing::Size(192, 26);
+			this->создатьToolStripMenuItem->Text = L"Создать";
+			// 
+			// сохранитьКакToolStripMenuItem
+			// 
+			this->сохранитьКакToolStripMenuItem->Name = L"сохранитьКакToolStripMenuItem";
+			this->сохранитьКакToolStripMenuItem->Size = System::Drawing::Size(192, 26);
+			this->сохранитьКакToolStripMenuItem->Text = L"Сохранить как";
+			// 
+			// очиститьToolStripMenuItem
+			// 
+			this->очиститьToolStripMenuItem->Name = L"очиститьToolStripMenuItem";
+			this->очиститьToolStripMenuItem->Size = System::Drawing::Size(192, 26);
+			this->очиститьToolStripMenuItem->Text = L"Очистить";
+			// 
+			// настройкиЦветаToolStripMenuItem
+			// 
+			this->настройкиЦветаToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->свободныйToolStripMenuItem,
+					this->ступенчатыйToolStripMenuItem
+			});
+			this->настройкиЦветаToolStripMenuItem->Name = L"настройкиЦветаToolStripMenuItem";
+			this->настройкиЦветаToolStripMenuItem->Size = System::Drawing::Size(141, 24);
+			this->настройкиЦветаToolStripMenuItem->Text = L"Настройки цвета";
+			// 
+			// свободныйToolStripMenuItem
+			// 
+			this->свободныйToolStripMenuItem->Name = L"свободныйToolStripMenuItem";
+			this->свободныйToolStripMenuItem->Size = System::Drawing::Size(182, 26);
+			this->свободныйToolStripMenuItem->Text = L"Свободный";
+			// 
+			// ступенчатыйToolStripMenuItem
+			// 
+			this->ступенчатыйToolStripMenuItem->Name = L"ступенчатыйToolStripMenuItem";
+			this->ступенчатыйToolStripMenuItem->Size = System::Drawing::Size(182, 26);
+			this->ступенчатыйToolStripMenuItem->Text = L"Ступенчатый";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->groupBox2);
+			this->groupBox1->Controls->Add(this->groupBox4);
+			this->groupBox1->Controls->Add(this->groupBox3);
+			this->groupBox1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->groupBox1->Location = System::Drawing::Point(0, 28);
+			this->groupBox1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Padding = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->groupBox1->Size = System::Drawing::Size(1262, 153);
+			this->groupBox1->TabIndex = 1;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Enter += gcnew System::EventHandler(this, &MyForm::groupBox1_Enter);
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->splitContainer2);
+			this->groupBox2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->groupBox2->Location = System::Drawing::Point(264, 17);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(512, 134);
+			this->groupBox2->TabIndex = 3;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"Кисть/Фигуры";
+			// 
+			// splitContainer2
+			// 
+			this->splitContainer2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->splitContainer2->Location = System::Drawing::Point(3, 18);
+			this->splitContainer2->Name = L"splitContainer2";
+			this->splitContainer2->Size = System::Drawing::Size(506, 113);
+			this->splitContainer2->SplitterDistance = 244;
+			this->splitContainer2->TabIndex = 0;
+			// 
+			// groupBox4
+			// 
+			this->groupBox4->Controls->Add(this->splitContainer1);
+			this->groupBox4->Dock = System::Windows::Forms::DockStyle::Right;
+			this->groupBox4->Location = System::Drawing::Point(776, 17);
+			this->groupBox4->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->groupBox4->Name = L"groupBox4";
+			this->groupBox4->Padding = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->groupBox4->Size = System::Drawing::Size(483, 134);
+			this->groupBox4->TabIndex = 2;
+			this->groupBox4->TabStop = false;
+			this->groupBox4->Text = L"Цвет кисти";
+			// 
+			// splitContainer1
+			// 
+			this->splitContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->splitContainer1->Location = System::Drawing::Point(3, 17);
+			this->splitContainer1->Name = L"splitContainer1";
+			// 
+			// splitContainer1.Panel2
+			// 
+			this->splitContainer1->Panel2->Controls->Add(this->groupBox5);
+			this->splitContainer1->Size = System::Drawing::Size(477, 115);
+			this->splitContainer1->SplitterDistance = 175;
+			this->splitContainer1->TabIndex = 0;
+			// 
+			// groupBox5
+			// 
+			this->groupBox5->Controls->Add(this->button7);
+			this->groupBox5->Controls->Add(this->button6);
+			this->groupBox5->Controls->Add(this->button5);
+			this->groupBox5->Controls->Add(this->button4);
+			this->groupBox5->Controls->Add(this->button3);
+			this->groupBox5->Controls->Add(this->button2);
+			this->groupBox5->Controls->Add(this->button1);
+			this->groupBox5->Dock = System::Windows::Forms::DockStyle::Top;
+			this->groupBox5->Location = System::Drawing::Point(0, 0);
+			this->groupBox5->Margin = System::Windows::Forms::Padding(10);
+			this->groupBox5->Name = L"groupBox5";
+			this->groupBox5->Size = System::Drawing::Size(298, 114);
+			this->groupBox5->TabIndex = 0;
+			this->groupBox5->TabStop = false;
+			this->groupBox5->Text = L"Палитра";
+			this->groupBox5->Enter += gcnew System::EventHandler(this, &MyForm::groupBox5_Enter);
+			// 
+			// button7
+			// 
+			this->button7->BackColor = System::Drawing::Color::Purple;
+			this->button7->Location = System::Drawing::Point(253, 21);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(35, 35);
+			this->button7->TabIndex = 6;
+			this->button7->UseVisualStyleBackColor = false;
+			// 
+			// button6
+			// 
+			this->button6->BackColor = System::Drawing::Color::Blue;
+			this->button6->Location = System::Drawing::Point(212, 21);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(35, 35);
+			this->button6->TabIndex = 5;
+			this->button6->UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this->button5->BackColor = System::Drawing::Color::Cyan;
+			this->button5->Location = System::Drawing::Point(171, 21);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(35, 35);
+			this->button5->TabIndex = 4;
+			this->button5->UseVisualStyleBackColor = false;
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::Color::Lime;
+			this->button4->Location = System::Drawing::Point(130, 21);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(35, 35);
+			this->button4->TabIndex = 3;
+			this->button4->UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::Yellow;
+			this->button3->Location = System::Drawing::Point(89, 21);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(35, 35);
+			this->button3->TabIndex = 2;
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(48, 21);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(35, 35);
+			this->button2->TabIndex = 1;
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Red;
+			this->button1->Location = System::Drawing::Point(7, 22);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(35, 35);
+			this->button1->TabIndex = 0;
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// groupBox3
+			// 
+			this->groupBox3->Dock = System::Windows::Forms::DockStyle::Left;
+			this->groupBox3->Location = System::Drawing::Point(3, 17);
+			this->groupBox3->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->groupBox3->Name = L"groupBox3";
+			this->groupBox3->Padding = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->groupBox3->Size = System::Drawing::Size(261, 134);
+			this->groupBox3->TabIndex = 1;
+			this->groupBox3->TabStop = false;
+			this->groupBox3->Text = L"Набор звуков";
+			this->groupBox3->Enter += gcnew System::EventHandler(this, &MyForm::groupBox3_Enter);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(120, 120);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Dpi;
+			this->ClientSize = System::Drawing::Size(1262, 673);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox2->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer2))->EndInit();
+			this->splitContainer2->ResumeLayout(false);
+			this->groupBox4->ResumeLayout(false);
+			this->splitContainer1->Panel2->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->EndInit();
+			this->splitContainer1->ResumeLayout(false);
+			this->groupBox5->ResumeLayout(false);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void toolStripMenuItem1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void groupBox1_Enter(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void groupBox3_Enter(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void groupBox5_Enter(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
