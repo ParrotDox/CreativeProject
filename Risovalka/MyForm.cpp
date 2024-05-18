bb6f96c31a04ad90ef998999b7fd382f59fab7ec
Danil Kolosv <Sheernoknignik@gmail.com>
2024-05-18 12:41:05 +0300

    Добавили конструктор формы

---
+++ Risovalka/MyForm.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^)
+{
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	Risovalka::MyForm form;
+	Application::Run(% form);
+}
