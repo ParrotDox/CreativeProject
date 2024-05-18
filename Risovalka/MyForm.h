05cc0a4f5979918071d49f1f31ff895bd35e08b0
Danil Kolosv <Sheernoknignik@gmail.com>
2024-05-18 14:28:03 +0300

    Шапка программы

--- Risovalka/MyForm.h
+++ Risovalka/MyForm.h
@@ -34,12 +34,65 @@ namespace Risovalka {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^ äåéñòâèÿToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ îÏğîãğàììåToolStripMenuItem;
+
+	private: System::Windows::Forms::ToolStripMenuItem^ ñîçäàòüToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ ñîõğàíèòüÊàêToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ ìåíşİëåìåíòîâToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ ñâîáîäíûéToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ ñòóïåíüêèToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ îÏğîãğàììåToolStripMenuItem1;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+
+	private: System::Windows::Forms::GroupBox^ groupBox3;
+	private: System::Windows::Forms::GroupBox^ groupBox2;
+	private: System::Windows::Forms::GroupBox^ groupBox4;
+
+
+
+
+
+
+
+	protected:
+
+	protected:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	protected:
+
+
+
+
+	protected:
+
+	private: System::ComponentModel::IContainer^ components;
+
+	protected:
+
+	protected:
 
 	private:
 		/// <summary>
 		/// Îáÿçàòåëüíàÿ ïåğåìåííàÿ êîíñòğóêòîğà.
 		/// </summary>
-		System::ComponentModel::Container^ components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +101,166 @@ namespace Risovalka {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300, 300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->äåéñòâèÿToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->îÏğîãğàììåToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ñîçäàòüToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ñîõğàíèòüÊàêToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ìåíşİëåìåíòîâToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ñâîáîäíûéToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ñòóïåíüêèToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->îÏğîãğàììåToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox4 = (gcnew System::Windows::Forms::GroupBox());
+			this->menuStrip1->SuspendLayout();
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->BackColor = System::Drawing::SystemColors::Control;
+			this->menuStrip1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->menuStrip1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->äåéñòâèÿToolStripMenuItem,
+					this->îÏğîãğàììåToolStripMenuItem, this->îÏğîãğàììåToolStripMenuItem1
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1280, 27);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// äåéñòâèÿToolStripMenuItem
+			// 
+			this->äåéñòâèÿToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->ñîçäàòüToolStripMenuItem,
+					this->ñîõğàíèòüÊàêToolStripMenuItem, this->ìåíşİëåìåíòîâToolStripMenuItem
+			});
+			this->äåéñòâèÿToolStripMenuItem->Name = L"äåéñòâèÿToolStripMenuItem";
+			this->äåéñòâèÿToolStripMenuItem->Size = System::Drawing::Size(51, 20);
+			this->äåéñòâèÿToolStripMenuItem->Text = L"Õîëñò";
+			// 
+			// îÏğîãğàììåToolStripMenuItem
+			// 
+			this->îÏğîãğàììåToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->ñâîáîäíûéToolStripMenuItem,
+					this->ñòóïåíüêèToolStripMenuItem
+			});
+			this->îÏğîãğàììåToolStripMenuItem->Name = L"îÏğîãğàììåToolStripMenuItem";
+			this->îÏğîãğàììåToolStripMenuItem->Size = System::Drawing::Size(90, 20);
+			this->îÏğîãğàììåToolStripMenuItem->Text = L"Ğåæèì öâåòà";
+			// 
+			// ñîçäàòüToolStripMenuItem
+			// 
+			this->ñîçäàòüToolStripMenuItem->Name = L"ñîçäàòüToolStripMenuItem";
+			this->ñîçäàòüToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->ñîçäàòüToolStripMenuItem->Text = L"Ñîçäàòü";
+			// 
+			// ñîõğàíèòüÊàêToolStripMenuItem
+			// 
+			this->ñîõğàíèòüÊàêToolStripMenuItem->Name = L"ñîõğàíèòüÊàêToolStripMenuItem";
+			this->ñîõğàíèòüÊàêToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->ñîõğàíèòüÊàêToolStripMenuItem->Text = L"Ñîõğàíèòü êàê";
+			// 
+			// ìåíşİëåìåíòîâToolStripMenuItem
+			// 
+			this->ìåíşİëåìåíòîâToolStripMenuItem->Name = L"ìåíşİëåìåíòîâToolStripMenuItem";
+			this->ìåíşİëåìåíòîâToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->ìåíşİëåìåíòîâToolStripMenuItem->Text = L"Ìåíş ıëåìåíòîâ";
+			// 
+			// ñâîáîäíûéToolStripMenuItem
+			// 
+			this->ñâîáîäíûéToolStripMenuItem->Name = L"ñâîáîäíûéToolStripMenuItem";
+			this->ñâîáîäíûéToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->ñâîáîäíûéToolStripMenuItem->Text = L"Ñâîáîäíûé";
+			// 
+			// ñòóïåíüêèToolStripMenuItem
+			// 
+			this->ñòóïåíüêèToolStripMenuItem->Name = L"ñòóïåíüêèToolStripMenuItem";
+			this->ñòóïåíüêèToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->ñòóïåíüêèToolStripMenuItem->Text = L"Ñòóïåí÷àòûé";
+			// 
+			// îÏğîãğàììåToolStripMenuItem1
+			// 
+			this->îÏğîãğàììåToolStripMenuItem1->Name = L"îÏğîãğàììåToolStripMenuItem1";
+			this->îÏğîãğàììåToolStripMenuItem1->Size = System::Drawing::Size(94, 20);
+			this->îÏğîãğàììåToolStripMenuItem1->Text = L"Î ïğîãğàììå";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->groupBox3);
+			this->groupBox1->Controls->Add(this->groupBox4);
+			this->groupBox1->Controls->Add(this->groupBox2);
+			this->groupBox1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->groupBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->groupBox1->Location = System::Drawing::Point(0, 27);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(1280, 100);
+			this->groupBox1->TabIndex = 1;
+			this->groupBox1->TabStop = false;
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Dock = System::Windows::Forms::DockStyle::Left;
+			this->groupBox2->Location = System::Drawing::Point(3, 22);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(425, 75);
+			this->groupBox2->TabIndex = 2;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"Ìóçûêàëüíûé èíñòğóìåíò";
+			// 
+			// groupBox3
+			// 
+			this->groupBox3->Dock = System::Windows::Forms::DockStyle::Right;
+			this->groupBox3->Location = System::Drawing::Point(852, 22);
+			this->groupBox3->Name = L"groupBox3";
+			this->groupBox3->Size = System::Drawing::Size(425, 75);
+			this->groupBox3->TabIndex = 3;
+			this->groupBox3->TabStop = false;
+			this->groupBox3->Text = L"Ïàëèòğà";
+			// 
+			// groupBox4
+			// 
+			this->groupBox4->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->groupBox4->Location = System::Drawing::Point(428, 22);
+			this->groupBox4->Name = L"groupBox4";
+			this->groupBox4->Size = System::Drawing::Size(849, 75);
+			this->groupBox4->TabIndex = 3;
+			this->groupBox4->TabStop = false;
+			this->groupBox4->Text = L"Êèñòü / Ôèãóğà";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1280, 720);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->groupBox1->ResumeLayout(false);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+
+
+
+
+
+
+
+
+
+
+
+};
 }
